d5415f6af764348188fc26961b74ab75cf78b163
Albert Byrone <albertbyrone1677@gmail.com>
2022-06-17 15:34:09 +0300

    solce the challange

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,37 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  # Implement the CRUD functionalities
+  get '/todos' do
+    todos = Todo.all.order(created_at: :desc).limit(10)
+    todos.to_json
   end
 
+  get '/category' do
+    category = Category.all.order(created_at: :desc)
+    category.to_json
+  end
+
+  post '/todo' do
+    todo = Todo.create(task: params[:task])
+    puts todo.task
+    todo.to_json
+  end
+
+  get '/todo/:id' do
+    todo = Todo.find(params[:id])
+    todo.to_json
+  end
+
+  delete '/todo/:id' do
+    todo = Todo.find(params[:id])
+    todo.destroy
+    { message: 'Todo deleted' }.to_json
+  end
+
+  patch '/todo/:id' do
+    todo = Todo.find(params[:id])
+    todo.update(task: params[:task])
+    todo.to_json
+  end
 end
